b4d33f80ecbbc37910c3f240b3912ddd4a14a4c4
Ignacio Arias <160803598+Cuzco1301@users.noreply.github.com>
2025-10-06 19:46:28 -0600

    Update customers_high_value.sql

--- sql_scripts/customers_high_value.sql
+++ sql_scripts/customers_high_value.sql
@@ -1,11 +1,16 @@
 --Show customers who have spent more than $100 in total.
-select 
-c.first_name ||' '||c.last_name as full_name, sum(amount) as total_spent
-from 
-customer as c
-join 
-payment as p using (customer_id)
-group by c.customer_id, full_name
-having sum(amount)>100
-order by sum(amount)desc
+SELECT
+    c.first_name || ' ' || c.last_name AS full_name,
+    SUM(amount) AS total_spent
+FROM
+    customer AS c
+JOIN
+    payment AS p USING (customer_id)
+GROUP BY
+    c.customer_id, full_name
+HAVING
+    SUM(amount) > 100
+ORDER BY
+    total_spent DESC;
+
 
