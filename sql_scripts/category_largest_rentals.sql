ca7dd27cca7313eee75d7ae0a6a9c0aea9e1a78c
Ignacio Arias <160803598+Cuzco1301@users.noreply.github.com>
2025-10-06 18:54:19 -0600

    Update category_largest_rentals.sql

--- sql_scripts/category_largest_rentals.sql
+++ sql_scripts/category_largest_rentals.sql
@@ -1,11 +1,15 @@
 -- category with the largest number of rented movies
-select c.name as category_name, count(r) as total_rentals
-from category as c
-join film_category as fc using (category_id)
-join inventory as i using (film_id)
-join rental as r using (inventory_id)
-group by c.name
-order by 
-total_rentals desc
+SELECT
+    c.name AS category_name,
+    COUNT(r.rental_id) AS total_rentals
+FROM
+    category AS c
+JOIN film_category AS fc USING (category_id)
+JOIN inventory AS i USING (film_id)
+JOIN rental AS r USING (inventory_id)
+GROUP BY
+    c.name
+ORDER BY
+    total_rentals DESC;
 
 
